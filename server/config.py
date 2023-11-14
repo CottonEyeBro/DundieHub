ced554700a88175f1b729a8aaf04eb0648c8a902
Cooper <55swimmer55@gmail.com>
2023-11-14 16:53:21 -0500

    Successful first init of db with seeded data for User get route

--- server/config.py
+++ server/config.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+# from models import User
 
 # Local imports
 
--- server/models.py
+++ server/models.py
@@ -3,10 +3,11 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from config import db
 
-convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"}
-metadata = MetaData(naming_convention=convention)
-db = SQLAlchemy(metadata=metadata)
+# convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"}
+# metadata = MetaData(naming_convention=convention)
+# db = SQLAlchemy(metadata=metadata)
 
 
 
