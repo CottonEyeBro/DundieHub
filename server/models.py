0181ba8cc99f772c45fca4c98d77394df40e7556
Cooper <55swimmer55@gmail.com>
2023-11-14 13:55:49 -0500

    Initialize base structure for models.py file classes

--- server/models.py
+++ server/models.py
@@ -1,6 +1,36 @@
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-from config import db
+convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+
+
+
+class User_Group(db.Model, SerializerMixin):
+    __tablename__ = 'user_groups'
+
+
+
+
+class Group(db.Model, SerializerMixin):
+    __tablename__ = 'groups'
+
+
+
+
+class Post(db.Model, SerializerMixin):
+    __tablename__ = 'posts'
+
+
+
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = 'comments'
