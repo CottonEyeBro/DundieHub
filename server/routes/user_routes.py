9ba50f1dc34f6513393ba4c4acc08d7d3a3501e4
Cooper <55swimmer55@gmail.com>
2023-11-15 17:52:24 -0500

    completed CRUD for users and users/1 routes

--- server/routes/user_routes.py
+++ server/routes/user_routes.py
@@ -41,12 +41,52 @@ def users():
 
     return response
 
-@app.route('/users/<int:id>', methods = ['GET'])
+@app.route('/users/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def user_by_id(id):
     user = User.query.filter(User.id == id).first()
+
+    if user:
+        if request.method == 'GET':
     
-    response = make_response(
-        user.to_dict(),
-        200
-    )
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            form_data = request.get_json()
+
+            try:
+                for attr in form_data:
+                    setattr(user, attr, form_data.get(attr))
+                
+                db.session.commit()
+
+                response = make_response(
+                    user.to_dict(),
+                    202
+                )
+
+            except ValueError:
+                response = make_response(
+                    {"errors": ["validation errors in PATCH to users id"]},
+                    400
+                )
+                return response
+            
+        elif request.method == 'DELETE':
+            db.session.delete(user)
+            db.session.commit()
+
+            response = make_response(
+                {},
+                204
+            )
+        
+    else:
+        response = make_response(
+            {"error": "User not found"},
+            404
+        )
+
     return response
