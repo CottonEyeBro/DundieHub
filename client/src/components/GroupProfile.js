3a23b67b69db06dcf4647d487c99a708683a278e
Cooper <55swimmer55@gmail.com>
2023-11-29 13:01:48 -0500

    Add styling and gif to Group Profile page

--- client/src/components/GroupProfile.js
+++ client/src/components/GroupProfile.js
@@ -2,10 +2,15 @@ import React from "react";
 
 function GroupProfile() {
 
-
     return (
         <div className="group-profile">
-            <h1>Group Profile Page</h1>
+            <div className="group-profile-div">
+                <h1>Group Profile Page</h1>
+                <div className="chili-div">
+                    <h2>Oops! Sorry to "spill the beans" but this page is still under construction...</h2>
+                    <iframe src="https://giphy.com/embed/SZQBPO4NqHkh6wmdXk" className="kevin-chili" allowFullScreen></iframe>
+                </div>
+            </div>
         </div>
     );
 }
