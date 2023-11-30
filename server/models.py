d1278fc1bc6b566f262ca1e760b8a057becea3f2
Cooper <55swimmer55@gmail.com>
2023-11-29 21:59:11 -0500

    Add default profile photo of DM logo for new users without affecting sign-up functionality or any other user interactions

--- server/models.py
+++ server/models.py
@@ -20,7 +20,7 @@ class User(db.Model, SerializerMixin): # =======================================
     username = db.Column(db.String, unique = True)
     _password_hash = db.Column(db.String)
     joined_on = db.Column(db.DateTime, default = datetime.utcnow)
-    profile_image_url = db.Column(db.String)
+    profile_image_url = db.Column(db.String, default = './images/DMLogo.png')
 
 
     # Relationships
